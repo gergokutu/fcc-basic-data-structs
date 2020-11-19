31ae89c38d2538a36e449a31e2801b9215294b97
gergokutu <gergo.kutu@gmail.com>
2020-11-19 16:01:26 +0100

    Change array item with bracket notation

--- data_1.js
+++ data_1.js
@@ -1,2 +1,6 @@
 let yourArray = [1, "two", true, undefined, null];
 console.log("Length:", yourArray.length);
+
+let myArray = ["a", "b", "c", "d"];
+myArray[1] = "b - modified";
+console.log(myArray);
