bac049c3967fd31b1f5c97d4f4b5b34573b85634
gergokutu <gergo.kutu@gmail.com>
2020-11-20 17:47:08 +0100

    Check obj props

--- data_2.js
+++ data_2.js
@@ -54,3 +54,35 @@ console.log('userActivity object:', userActivity);
   foodsToRemove.map(item => delete foods[item]);
   console.log('Just apples, bananas and grapes >', foods);
 }
+
+// Check if an Object has a Property
+// 1. Obj.hasOwnProperty('propName') > boolean
+// 2. 'propName' in Obj > boolean
+let users = {
+  Alan: {
+    age: 27,
+    online: true
+  },
+  Jeff: {
+    age: 32,
+    online: true
+  },
+  Sarah: {
+    age: 48,
+    online: true
+  },
+  Ryan: {
+    age: 19,
+    online: true
+  }
+};
+
+function isEveryoneHere(obj) {
+  const keysToCheck = ['Alan', 'Jeff', 'Sarah', 'Ryan'];
+  for (let i = 0; i < keysToCheck.length; i++) {
+    // if (!users.hasOwnProperty(keysToCheck[i])) return false;
+    if (keysToCheck[i] in users === false) return false;
+  }
+  return true;
+};
+console.log('Should be true >', isEveryoneHere(users));
