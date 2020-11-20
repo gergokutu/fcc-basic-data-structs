128b4fcf4a95d28ed503da26b83bbefef372cbb7
gergokutu <gergo.kutu@gmail.com>
2020-11-20 17:23:40 +0100

    Access object props dynamically > bracket notation

--- data_2.js
+++ data_2.js
@@ -7,5 +7,34 @@ let foods = {
 foods['bananas'] = 13;
 foods['grapes'] = 35;
 foods.strawberries = 27;
+console.log('foods object:', foods);
 
-console.log(foods);
+// Nested object
+let userActivity = {
+  id: 23894201352,
+  date: 'January 1, 2017',
+  data: {
+    totalUsers: 51,
+    online: 42
+  }
+};
+
+userActivity.data.online = 45;
+console.log('userActivity object:', userActivity);
+
+// Access properties with bracket notation
+{
+  let foods = {
+    apples: 25,
+    oranges: 32,
+    plums: 28,
+    bananas: 13,
+    grapes: 35,
+    strawberries: 27
+  };
+
+  function checkInventory(scannedItem) {
+    return foods[scannedItem];
+  };
+  console.log('Apples should be 25 >', checkInventory("apples"));
+}
