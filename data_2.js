c471a65b5f44c77737afa98cab56edca009dd889
gergokutu <gergo.kutu@gmail.com>
2020-11-21 12:17:13 +0100

    Gather object property keys into an array

--- data_2.js
+++ data_2.js
@@ -113,3 +113,33 @@ console.log('Should be true >', isEveryoneHere(users));
 
   console.log('Should be 1 >', countOnline(users));
 }
+
+// Generate an array of all object keys
+{
+  let users = {
+    Alan: {
+      age: 27,
+      online: false
+    },
+    Jeff: {
+      age: 32,
+      online: true
+    },
+    Sarah: {
+      age: 48,
+      online: false
+    },
+    Ryan: {
+      age: 19,
+      online: true
+    }
+  };
+
+  function getArrayOfUsers(obj) {
+    return Object.keys(obj);
+  };
+
+  console.log('Should be [ "Alan", "Jeff", "Sarah", "Ryan" ] >',
+    getArrayOfUsers(users)
+  );
+}
